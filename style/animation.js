00cfd480b1434a51525ba32e21fb3cd77a071494
charley04310 <git>
2021-07-08 10:40:24 +0200

    darkmode

--- style/animation.js
+++ style/animation.js
@@ -1,3 +1,5 @@
+
+
 function SetModal(){
 
     var modal = document.getElementById("myModal");
@@ -24,4 +26,40 @@ function SetModal(){
         modal.style.display = "none";
       }
     }
+}
+
+
+function darkMode(){
+    var toggled =false;
+var headerContainer = document.getElementById("header-container");
+var switchHolder = document.getElementById("outer-div");
+var switchButton = document.getElementById("inner-div");
+
+switchButton.addEventListener('click',function () {
+    if(!toggled){
+    switchButton.style.transition =".4s all ease-in-out";
+    switchButton.style.transform ="translateX(48px)";
+    switchButton.style.backgroundColor = "white";
+    switchHolder.style.border= "2px solid white";
+    document.body.style.backgroundColor = "#252730f5";
+    headerContainer.style.backgroundColor = "#252730"
+
+    toggled = true;
+    }else{
+
+    switchButton.style.transition =".4s all ease-in-out";
+
+    headerContainer.style.backgroundColor = "#d6d6d6"
+
+    switchButton.style.transform ="translateX(-1px)";
+    switchButton.style.backgroundColor = "#f07d2d";
+    switchHolder.style.border= "2px solid #f07d2d";
+    document.body.style.backgroundColor = "white";
+    headerContainer.style.backgroundColor = "white"
+
+
+    toggled = false;
+    }
+});
+
 }
