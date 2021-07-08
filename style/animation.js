2bdae89af785ad49e30e9c511f22036d5b3bd7cb
charley04310 <git>
2021-07-08 11:24:22 +0200

    popup addarticle/ edit information

--- style/animation.js
+++ style/animation.js
@@ -1,5 +1,4 @@
 
-
 function SetModal(){
 
     var modal = document.getElementById("myModal");
@@ -27,6 +26,67 @@ function SetModal(){
       }
     }
 }
+
+
+
+function ModalAddArticle(){
+
+    var modal = document.getElementById("AddArticle");
+    
+    // Get the button that opens the modal
+    var btn = document.getElementById("AddPost");
+    
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[1];
+    
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+    
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+}
+
+
+function EditInf(){
+
+    var modal = document.getElementById("EditInf");
+    
+    // Get the button that opens the modal
+    var btn = document.getElementById("User");
+    
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[2];
+    
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+    
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+}
+
+
 
 
 function darkMode(){
