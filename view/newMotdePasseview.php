2938c4a60f1438e9c7b2bb33619bd5272fa9c643
Julie-34 <67684704+Julie-34@users.noreply.github.com>
2021-08-16 11:15:50 +0200

    ajout view new mot de passe

---
+++ view/newMotdePasseview.php
@@ -0,0 +1,19 @@
+
+<div class="container">
+<div class="d-flex flex-column align-items-center bg-transparent my-2">
+<form class="text-center border border-light w-50 p-5 " action="#!">
+<div  class="p-2 bg-transparent border">
+    <p style="text-align:left">Vous avez perdu votre mot de passe ou vous souhaitez le changer ?<br>
+Merci de renseigner votre email</p>
+
+    <input type="email" id="defaultLoginFormEmail" class="form-control mb-4" placeholder="Adresse mail">
+    
+</form>
+
+          <div class="container-fluid mb-5"> </div>
+      <div class="col bg-transparent"><a href="" style="color:black; text-decoration:none !important">changez votre mot de passe</a>
+      </div>
+
+</div>
+</div>
+</div>
