8b222cfffedb59222a25f2ea29a6c23994215733
Julie-34 <67684704+Julie-34@users.noreply.github.com>
2021-08-16 11:27:48 +0200

    modif view new mot de passe

--- view/newMotdePasseview.php
+++ view/newMotdePasseview.php
@@ -1,19 +1,32 @@
 
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+          <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+          <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+          <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+</head>
 <div class="container">
 <div class="d-flex flex-column align-items-center bg-transparent my-2">
-<form class="text-center border border-light w-50 p-5 " action="#!">
+<form class="text-center border border-light w-50 p-5 " action="controler/newMotdePasse.php">
 <div  class="p-2 bg-transparent border">
     <p style="text-align:left">Vous avez perdu votre mot de passe ou vous souhaitez le changer ?<br>
 Merci de renseigner votre email</p>
 
     <input type="email" id="defaultLoginFormEmail" class="form-control mb-4" placeholder="Adresse mail">
     
-</form>
+
 
           <div class="container-fluid mb-5"> </div>
-      <div class="col bg-transparent"><a href="" style="color:black; text-decoration:none !important">changez votre mot de passe</a>
+      <div class="col bg-transparent"><a href="" style="color:black; text-decoration:none !important">Envoyer</a>
       </div>
-
+</form>
 </div>
 </div>
 </div>
+</body>
+</html>
